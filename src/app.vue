96e5db27e24bb0595f44aae56dce2875fabb734c
persilee <i@lishaoy.net>
2025-01-24 12:38:47 +0800

    添加 Nuxt 应用程序的基本配置和文件结构

---
+++ src/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+console.log(config)
+</script>
