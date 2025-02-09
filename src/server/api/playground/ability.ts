159ffa2048cd6bace001f78a6c4fd93775afb6fb
persilee <i@lishaoy.net>
2025-02-09 22:36:15 +0800

    更新权限管理逻辑；添加标准用户、编辑和管理员的权限定义，支持用户信息更新和 Startup 管理

--- src/server/api/playground/ability.ts
+++ src/server/api/playground/ability.ts
@@ -1,3 +1,5 @@
+import { subject } from '@casl/ability'
+
 export default defineEventHandler(async (event) => {
   const {
     user,
@@ -8,6 +10,10 @@ export default defineEventHandler(async (event) => {
   const abilities = {
     canManageUser: ability.can('manage', 'User'),
     canDeleteUser: ability.can('delete', 'User'),
+    canManageStartup: ability.can('manage', 'Startup'),
+    canUpdateUser: ability.can('update', 'User'),
+    canUpdateEmail: ability.can('update', 'User', 'email'),
+    canUpdatePassword: ability.can('update', 'User', 'password'),
   }
 
   return { user, rules, abilities }
--- src/server/utils/ability.ts
+++ src/server/utils/ability.ts
@@ -1,5 +1,6 @@
 import { AbilityBuilder, createMongoAbility } from '@casl/ability'
-import type { MongoAbility } from '@casl/ability'
+import type { MongoAbility, ExtractSubjectType } from '@casl/ability'
+import { capitalize, get } from 'lodash-es'
 import { build } from 'nuxt'
 import type { Item } from '~/schema/user'
 
@@ -29,17 +30,43 @@ export type Subjects = 'User' | 'Startup' | 'all'
 export type AppAbility = MongoAbility<[Actions, Subjects]>
 
 /**
- * 为指定用户定义权限。
+ * 定义用户的权限。
  *
- * @param user - 要为其定义权限的用户。
- * @returns 构建的用户权限。
+ * @param user - 用户对象。
+ * @returns 构建的权限对象。
+ *
+ * @remarks
+ * 根据用户的角色（标准用户、编辑、管理员）定义不同的权限。
+ * - 标准用户可以更新自己的名称、邮箱和手机号码。
+ * - 编辑可以管理所有的 Startup。
+ * - 管理员可以管理所有内容。
+ *
+ * @example
+ * ```typescript
+ * const ability = defineAbilityFor(user);
+ * ```
  */
 export const defineAbilityFor = (user: Item) => {
   const { can, cannot, build } = new AbilityBuilder<AppAbility>(createMongoAbility)
+
+  if (isStandard(user)) {
+    can('update', 'User', ['name', 'email', 'mobile'], { id: { $eq: user.id as any } })
+  }
+
+  if (isEditor(user)) {
+    can('manage', 'Startup')
+  }
 
   if (isAdministrator(user)) {
     can('manage', 'all')
   }
 
-  return build()
+  return build({
+    detectSubjectType(subject) {
+      const id: string = get(subject, 'id') ?? ''
+      const subjectType = capitalize(id.split(':')[0]) as ExtractSubjectType<Subjects>
+
+      return subjectType
+    },
+  })
 }
