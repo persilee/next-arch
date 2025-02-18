027e3f37d74c933d7fd4dfead2d7a693f940169b
persilee <i@lishaoy.net>
2025-02-18 21:57:36 +0800

    添加文件删除功能；实现文件及其相关数据的删除逻辑，更新API以支持文件删除请求

---
+++ src/server/api/console/files/[id]/index.delete.ts
@@ -0,0 +1,37 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { StringRecordId } from 'surrealdb'
+import { params, type Row } from '~/schema/file'
+
+export default defineEventHandler(async (event) => {
+  const { id } = await getValidatedRouterParams(event, params.parse)
+  const result = await db.select<Row>(new StringRecordId(id))
+
+  if (!result) {
+    throw createError({ statusCode: 404 })
+  }
+
+  const { uid } = result
+  const filePath = path.join(fileSystemBase, uid)
+  const fileDerived = path.join(fileSystemBase, 'derived', uid)
+
+  try {
+    // 删除文件
+    await fs.access(filePath, fs.constants.F_OK)
+    await fs.unlink(filePath)
+    // 删除目录
+    await fs.access(fileDerived, fs.constants.F_OK)
+    await fs.rm(fileDerived, { recursive: true })
+    // 删除数据
+    const delRow = await db.delete(new StringRecordId(id))
+    if (!delRow) {
+      throw createError({ statusCode: 404, message: '删除数据失败' })
+    }
+    return { id }
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      message: '删除失败~',
+    })
+  }
+})
